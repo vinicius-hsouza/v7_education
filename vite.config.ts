e0e1b81e11010b6dca2938b1d4c2d365ff23a79c
Vinicius Henrique de Souza <vini.henrique2501@gmail.com>
2024-09-02 01:22:58 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    manifest: {
+      name: "React-vite-app",
+      short_name: "react-vite-app",
+      description: "I am a simple vite app",
+      theme_color: '#171717',
+      background_color: '#f0e7db',
+      display: "standalone",
+      scope: '/',
+      start_url: "/",
+      orientation: 'portrait'
+    }
+  })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
