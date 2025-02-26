ce37e9a2b0175c359a63fa55a8d8dee4ae9d02c2
Vinicius Henrique de Souza <vini.henrique2501@gmail.com>
2025-02-25 22:16:11 -0300

    fix appId

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,28 @@ export default defineConfig({
       scope: '/',
       start_url: "/",
       orientation: 'portrait',
-      icons: [
+      "icons": [
         {
-          src: './public/my_cash_icon.png',
-          sizes: '192x192',
-          type: 'image/png'
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
         },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
       ]
     }
   })],
